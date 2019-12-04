c49aa63182d3e148650bb6ae2e970d5652ec48c5
houa cha <itshoua@gmail.com>
2019-12-04 10:06:08 -0600

    added comments

--- Game.cs
+++ Game.cs
@@ -26,5 +26,14 @@ namespace lemonadestand
             UserInterface.Name(player.name);
             day.RunDay();
         }
+
+
+
+        // Two places where I think I incorperated S.O.L.I.D.:
+        // The first place I can think of is my inventory. I made methods for each individual items to be added to the players inventory,
+        // each with their own list to have it added to. By doing this I establish a single responsibily for each method, closed to change but
+        // open to adjustments, and maybe the other things (maybe).
+        // The second place would most likely be the player class. The player has methods that I think fits single responsibliy but after learning
+        // more about S.O.L.I.D. maybe I'm wrong.
     }
 }
